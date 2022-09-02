08c5d544a56aa85015b5cef43ed6e6e4fa2f6d6d
zhuangbinan <zhuangbinan1024@foxmail.com>
2022-09-02 18:32:04 +0800

    自己手写数组栈还没写完

---
+++ src/main/java/club/zhuangbinan/stack/ArrayStackDemo.java
@@ -0,0 +1,25 @@
+package club.zhuangbinan.stack;
+
+public class ArrayStackDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class ArrayStack {
+
+    private int maxSize;
+    private int[] stack;
+    private int top = -1;
+
+    public ArrayStack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new int[maxSize];
+    }
+
+//    public boolean isFull() {
+//        return top ==
+//    }
+
+}
