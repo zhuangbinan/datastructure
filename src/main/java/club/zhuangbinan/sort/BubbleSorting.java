0dd52197303553d56efce830e652d6b691613b69
zhuangbinan <zhuangbinan1024@foxmail.com>
2022-09-21 17:59:01 +0800

    bubbleSorting

---
+++ src/main/java/club/zhuangbinan/sort/BubbleSorting.java
@@ -0,0 +1,77 @@
+package club.zhuangbinan.sort;
+
+import java.util.Arrays;
+
+/**
+ * BubbleSorting by myself
+ */
+public class BubbleSorting {
+
+    public static void main(String[] args) {
+        int[] arr1 = {-1, 2, 10, 9, -2};
+        int[] arr2 = {-1, 2, 10, 9, -2};
+        int[] arr3 = {-1, 2, 9, 10, -2};
+
+        int[] arrt1 = {2,1,3,4,5};
+        int[] arrt2 = {2,1,3,4,5};
+
+        //双重for循环版本 O(n²)
+        normalBubbleSort(arr1,true);
+        betterBubbleSort(arr2,true);
+    }
+
+    /**
+     * normal bubble sort
+     * @param arr array that you will sort
+     * @param isShowProgress is show sorting progress
+     */
+    public static void normalBubbleSort(int[] arr, boolean isShowProgress) {
+        int temp = Integer.MIN_VALUE;
+        int sortCount = 0;
+        //内层for循环是一次冒泡排序的
+        //还要在外层加一层for循环驱动内层排序直到排序完成
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+                sortCount++;
+            }
+            if (isShowProgress) {
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+        System.out.printf("normalBubbleSort排序结果:%s\n",Arrays.toString(arr));
+        System.out.printf("normalBubbleSort排序次数:%s\n",sortCount);
+    }
+
+    /**
+     * better bubble sort
+     * @param arr array that you will sort
+     * @param isShowProgress is show sorting progress
+     */
+    public static void betterBubbleSort(int[] arr, boolean isShowProgress) {
+        int temp = Integer.MIN_VALUE;
+        int sortCount = 0;
+        //内层for循环是一次冒泡排序的
+        //还要在外层加一层for循环驱动内层排序直到排序完成
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int i = 0; i < arr.length - 1 - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+                sortCount++;
+            }
+            if (isShowProgress) {
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+        System.out.printf("betterBubbleSort排序结果:%s\n",Arrays.toString(arr));
+        System.out.printf("betterBubbleSort排序次数:%s\n",sortCount);
+    }
+
+}
